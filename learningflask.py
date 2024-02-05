9322e48879d39abbf067ab46d17e486f19fcbe05
Abdullah Abdur Rahim <ss5478802@gmail.com>
2024-02-05 15:18:52 +0300

    Initial commit

--- learningflask.py
+++ learningflask.py
@@ -1,1 +1,10 @@
 # Learning Flask microframework of Python
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/<name>")
+def hello_world(name):
+    return f"Hello {escape(name)}!"
