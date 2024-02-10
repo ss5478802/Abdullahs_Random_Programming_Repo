0ed562673058a0117b7ac4ef5ca6a22817cab706
Abdullah Abdur Rahim <ss5478802@gmail.com>
2024-02-10 08:36:57 +0300

    Initial commit

--- learningflask.py
+++ learningflask.py
@@ -1,8 +1,38 @@
 # Learning Flask microframework of Python
 from flask import Flask, render_template
 from markupsafe import escape
+from random import choice
 
 app = Flask(__name__)
+colours_for_other_tags = [
+    "aqua",
+    "black",
+    "blue",
+    "fuchsia",
+    "gray",
+    "green",
+    "lime",
+    "maroon",
+    "navy",
+    "olive",
+    "purple",
+    "red",
+    "teal",
+    "yellow",
+]
+colours_for_background = [
+    "white",
+    "ivory",
+    "lightblue",
+    "paleyellow",
+    "gainsboro",
+    "powderblue",
+    "lightyellow",
+    "peachpuff",
+    "lavender",
+    "antiquewhite",
+]
+
 
 
 @app.route("/")
@@ -12,17 +42,30 @@ def index():
 
 @app.route("/<name>/")
 def hello_world(name):
-    return f"Hello {escape(name)}!"
+    if name.strip() != "":
+        return render_template(
+            "helloWorld.html",
+            persons_name=name,
+            heading_colour=choice(colours_for_other_tags),
+            bgcolour=choice(colours_for_background),
+        )
+    else:
+        return (
+            "<h1 style='color:red;'>Name is missing. Enter your name in the URL!</h1>"
+        )
 
 
 @app.route("/<name>/<int:age>/")
-@app.route("/<int:age>/<name>")
+@app.route("/<int:age>/<name>/")
 @app.route("/<int:age>/")
 def age(age, name=None):
-    if name != None:
-        return f"Your name is {escape(name)} and ur age is {escape(age)}"
-    else:
-        return f"Your age is {escape(age)}"
+    return render_template(
+        "age.html",
+        persons_name=name,
+        persons_age=age,
+        heading_colour=choice(colours_for_other_tags),
+        bgcolour=choice(colours_for_background),
+    )
 
 
 if __name__ == "__main__":
